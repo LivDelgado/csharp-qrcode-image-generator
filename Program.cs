de8ec4d4c31c7d3be483b0bbd472b5b3f38fe1d6
Lívia Delgado <livia.carneiro@animaeducacao.com.br>
2020-05-21 10:25:59 -0300

    Add initial project structure

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace QRCodeGeneratorTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string qrtext = Console.ReadLine();
+            QRCodeUtil.GenerateFile(qrtext, "qrcodes/qrc.png");
+        }
+    }
+}
---
+++ QRCodeUtil.cs
@@ -0,0 +1,17 @@
+﻿using QRCoder;
+using System.Drawing;
+
+namespace QRCodeGeneratorTest
+{
+    public static class QRCodeUtil
+    {
+        public static void GenerateFile(string qrText, string fileName)
+        {
+            QRCodeGenerator qrGenerator = new QRCodeGenerator();
+            QRCodeData qrCodeData = qrGenerator.CreateQrCode(qrText, QRCodeGenerator.ECCLevel.Q);
+            QRCode qrCode = new QRCode(qrCodeData);
+            Bitmap qrCodeImage = qrCode.GetGraphic(20);
+            qrCodeImage.Save(fileName, System.Drawing.Imaging.ImageFormat.Png);
+        }
+    }
+}
